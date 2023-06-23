87e609d3f011bd7b6c8e7a6b89b8e68841f89286
Rohan kumar Ghanwar <rohankumarghanwar@MagicAir-M1.local>
2023-06-23 13:13:22 +0530

    delete folder

--- Router1x3/rd_top_agt/router_rd_seqr.sv
+++
@@ -1,9 +0,0 @@
-class router_rd_seqr extends uvm_sequencer #(read_xtn);
-`uvm_component_utils(router_rd_seqr)
-
-extern function new(string name="router_rd_seqr",uvm_component parent);
-endclass
-
-function router_rd_seqr::new(string name="router_rd_seqr",uvm_component parent);
-	super.new(name,parent);
-endfunction
